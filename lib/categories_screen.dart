0ba30cb5de930692077e5d4bf0bcddfc277b16d3
artemychych <artemychmain@gmail.com>
2020-12-07 20:58:14 +0900

    Styling main page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+       padding: const EdgeInsets.all(25), 
+       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-      children: [
+      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
 
-      ],
-
+    ),
     );
   }
 }
