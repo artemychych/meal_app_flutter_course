4b708072b30953280618ba52cc27790e0e04e3a6
artemychych <artemychmain@gmail.com>
2020-12-07 21:30:34 +0900

    Categories with adaptive description added

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,14 +11,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
-       padding: const EdgeInsets.all(25), 
+       padding: const EdgeInsets.all(25),
        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
+      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(
+          catData.id,
+          catData.title,
+          catData.color,
+      ),
+      )
+          .toList(),
 
     ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/category_meals_screen.dart';
 import 'categories_screen.dart';
 
 
@@ -31,6 +32,9 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
