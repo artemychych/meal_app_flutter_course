54369f0756033fafa90e8456952121b8dd2cde30
artemychych <artemychmain@gmail.com>
2020-12-11 02:58:58 +0900

    Categories with title

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,10 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
   }
